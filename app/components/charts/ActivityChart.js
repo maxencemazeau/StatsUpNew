1295bd6d11897a0df4963d5c2cd6a9b31dfb6c83
unknown <maxencemazeau@outlook.fr>
2024-08-15 18:07:28 -0400

    Every charts

--- app/components/charts/ActivityChart.js
+++ app/components/charts/ActivityChart.js
@@ -12,13 +12,13 @@ export default function ActivityChart({ UserId, ChartTimeFrame, ActivityId }) {
     const screenWidth = Dimensions.get("window").width;
     const [chartLabel, setChartLabel] = useState([])
     const [xDataArray, setXDataArray] = useState([])
-    const thisMonthLabel = []
+    const [segmentNumber, setSegmentNumber] = useState(0)
+    const [fromZero, setFromZero] = useState(false)
     let i = 1
 
     useEffect(() => {
         const getActivityData = async () => {
             const response = await axios.get(getActivityChartData, { params: { UserId: UserId, ChartFrame: ChartTimeFrame, ActivityId: ActivityId } })
-            console.log(response.data)
             setChartData(response.data)
         }
 
@@ -29,8 +29,10 @@ export default function ActivityChart({ UserId, ChartTimeFrame, ActivityId }) {
     useEffect(() => {
         const createData = () => {
             setXDataArray([])
-            setChartLabel(weekDay)
+            setChartLabel([])
+            let newXDataArray = []
             let matching = false
+            let NbActivity = 0
             switch (ChartTimeFrame) {
                 case 1:
                     setChartLabel(weekDay)
@@ -41,16 +43,17 @@ export default function ActivityChart({ UserId, ChartTimeFrame, ActivityId }) {
                             let newDate = new Date(chartDate)
                             let theDay = newDate.getUTCDay() - 1
                             if (parseInt(theDay) === i) {
-                                let NbActivity = chartData[j].NbActivity
-                                setXDataArray(prevState => [...prevState, NbActivity])
+                                NbActivity = chartData[j].NbActivity
+                                newXDataArray.push(NbActivity)
                                 matching = true
                             }
                         }
 
                         if (!matching) {
-                            setXDataArray(prevState => [...prevState, 0])
+                            newXDataArray.push(0)
                         }
                     }
+                    setXDataArray(newXDataArray)
                     break;
                 case 2:
                     setChartLabel("")
@@ -63,27 +66,21 @@ export default function ActivityChart({ UserId, ChartTimeFrame, ActivityId }) {
                         matching = false
                         for (j = 0; j < chartData.length; j++) {
                             let chartDate = chartData[j].TimeStamp
-                            let NbActivity = chartData[j].NbActivity
+                            NbActivity = chartData[j].NbActivity
                             chartDate = new Date(chartDate).getDate()
                             if (chartDate === i) {
-                                setXDataArray(prevState => [...prevState, NbActivity])
+                                newXDataArray.push(NbActivity)
                                 matching = true
                             }
                         }
                         if (!matching) {
-                            setXDataArray(prevState => [...prevState, 0])
+                            newXDataArray.push(0)
                         }
 
-                        if (dayMax < 25) {
-                            if (i === halfMonthDate || i === 1 || i === dayMax) {
-                                newArrayTest.push(i)
-                            } else {
-                                newArrayTest.push("")
-                            }
-                        }
                         i++
                     }
-                    setChartLabel(newArrayTest)
+                    setXDataArray(newXDataArray)
+                    setChartLabel(months)
                     break;
                 case 3:
                     setChartLabel(months)
@@ -91,26 +88,66 @@ export default function ActivityChart({ UserId, ChartTimeFrame, ActivityId }) {
                         matching = false
                         for (j = 0; j < chartData.length; j++) {
                             let theMonth = chartData[j].TimeStamp.substring(6, 7)
-                            if (parseInt(theMonth) === i) {
-                                let NbActivity = chartData[j].NbActivity
-                                setXDataArray(prevState => [...prevState, NbActivity])
+                            if (parseInt(theMonth) === i + 1) {
+                                NbActivity = chartData[j].NbActivity
+                                newXDataArray.push(NbActivity)
                                 matching = true
                             }
                         }
 
                         if (!matching) {
-                            setXDataArray(prevState => [...prevState, 0])
+                            newXDataArray.push(0)
                         }
                     }
+                    setXDataArray(newXDataArray)
                     break;
                 case 4:
+                    const today = new Date();
+                    const currentMonth = today.getMonth(); // 0-11 (Janvier est 0)
 
+                    for (let i = 0; i < 3; i++) {
+                        matching = false
+                        const monthIndex = (currentMonth - i + 12) % 12;
+                        setChartLabel(prevState => [...prevState, months[monthIndex]])
+                        for (j = 0; j < chartData.length; j++) {
+                            NbActivity = chartData[j].NbActivity
+                            const realMonthIndex = monthIndex + 1
+
+                            let chartMonth = chartData[j].TimeStamp.substring(6, 7)
+
+                            if (parseInt(chartMonth) === realMonthIndex) {
+                                newXDataArray.push(NbActivity)
+                                matching = true
+                            }
+                        }
+                        if (!matching) {
+                            newXDataArray.push(0)
+                        }
+                    }
+                    setXDataArray(newXDataArray)
                     break;
-
+                case 5:
+                    let year = ""
+                    setChartLabel(prevState => [...prevState, ""])
+                    setXDataArray(prevState => [...prevState, 0])
+                    for (i = 0; i < chartData.length; i++) {
+                        NbActivity = chartData[i].NbActivity
+                        year = chartData[i].TimeStamp
+                        newXDataArray.push(NbActivity)
+                        setXDataArray(prevState => [...prevState, parseInt(NbActivity)])
+                        setChartLabel(prevState => [...prevState, year])
+                    }
+                    break
+            }
+            let maxNbActivity = Math.max(...newXDataArray.map(item => item))
+            if (maxNbActivity === 1) {
+                setSegmentNumber(1)
+                setFromZero(false)
+            } else {
+                setSegmentNumber(maxNbActivity)
+                setFromZero(true)
             }
         }
-
-        console.log(xDataArray)
 
         createData()
     }, [chartData])
@@ -129,22 +166,31 @@ export default function ActivityChart({ UserId, ChartTimeFrame, ActivityId }) {
 
     return (
         <>
-            <LineChart
-                data={data}
-                width={screenWidth} // from react-native
-                height={300}
-                chartConfig={{
-                    backgroundColor: "#191919",
-                    backgroundGradientFrom: "#191919",
-                    backgroundGradientTo: "#191919",
-                    color: (opacity = 1) => `rgba(221, 122, 52, ${opacity})`,
-                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                    style: {
-                        borderRadius: 16
-                    },
-                }}
-                bezier
-            />
+            {xDataArray.length > 0 && chartLabel.length > 0 && (
+                <LineChart
+                    data={data}
+                    width={screenWidth - 20} // from react-native
+                    height={300}
+                    chartConfig={{
+                        backgroundColor: "#191919",
+                        backgroundGradientFrom: "#191919",
+                        decimalPlaces: 0,
+                        propsForBackgroundLines: {
+                            strokeDasharray: '' // Make sure background lines are not visible if not needed
+                        },
+                        backgroundGradientTo: "#191919",
+                        color: (opacity = 1) => `rgba(221, 122, 52, ${opacity})`,
+                        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        style: {
+                            borderRadius: 16
+                        },
+                    }}
+                    segments={segmentNumber}
+                    withInnerLines={false} // add this line
+                    fromZero={fromZero}
+                    bezier
+                />
+            )}
         </>
     )
 }
