17c0ee39a6d5b395c672e4734a67968e83d7eb6a
unknown <maxencemazeau@outlook.fr>
2024-09-15 19:00:09 -0400

    redux persist login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack, Slot } from 'expo-router';
 import { Provider } from 'react-redux';
-import { store } from './reduxState/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './reduxState/store';
 import { QueryClient, QueryClientProvider } from "react-query";
 import { useEffect } from 'react';
 import { TamaguiProvider } from 'tamagui';
@@ -27,9 +28,11 @@ export default function IndexLayout() {
   return (
     <TamaguiProvider config={config}>
       <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <Stack screenOptions={{ headerShown: false }} />
-        </QueryClientProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <QueryClientProvider client={queryClient}>
+            <Stack screenOptions={{ headerShown: false }} />
+          </QueryClientProvider>
+        </PersistGate>
       </Provider>
     </TamaguiProvider>
   );
--- app/reduxState/store.js
+++ app/reduxState/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist"
+import storage from "@react-native-async-storage/async-storage"
 import navigationReducer from "./navigation/navigationSlice"
 import activityOffsetReducer from "./offset/activityOffsetSlice"
 import goalOffsetReducer from "./offset/goalOffsetSlice";
@@ -15,6 +17,13 @@ import showDelete from "./popUp/showDelete";
 import RoutingReducer from "./navigation/routingSlice"
 import timerReducer from './timer/timer'
 
+const loginPersistConfig = {
+    key: 'login',
+    storage: storage
+}
+
+const persistLoginReducer = persistReducer(loginPersistConfig, loginReducer)
+
 export const store = configureStore({
     reducer: {
         navigation: navigationReducer,
@@ -22,7 +31,7 @@ export const store = configureStore({
         goalOffset: goalOffsetReducer,
         hasMoreActivityData: hasMoreActivityDataReducer,
         hasMoreGoalData: hasMoreGoalDataReducer,
-        login: loginReducer,
+        login: persistLoginReducer,
         isActivityLoading: isActivityLoadingReducer,
         isGoalLoading: isGoalLoadingReducer,
         loadingError: loadingErrorReducer,
@@ -32,5 +41,14 @@ export const store = configureStore({
         globalNavigation: RoutingReducer,
         timer: timerReducer
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                // Ignore actions related to redux-persist
+                ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+            },
+        }),
 });
+
+export const persistor = persistStore(store)
 
